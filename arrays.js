7213e318a0a61698953aa6e4c3b577ece3e4aba6
Travis Exline <t.exline22@gmail.com>
2019-04-02 19:40:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
-function addElementToEndOfArray (array, item) {
-
+function addElementToEndOfArray (array, element) {
+  array.push('foo');
+  return array
 }
