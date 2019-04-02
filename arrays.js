2451f8e676fdbdde7fe51e14426a0cb0c7ed7fcf
Travis Exline <t.exline22@gmail.com>
2019-04-02 17:04:13 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat",
+"skittles"];
+
+function addElementToBeginningOfArray (array, element) {
+  return ["foo", ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift("foo");
+  return array
+}
+
+function addElementToEndOfArray (array, item) {
+
+}
